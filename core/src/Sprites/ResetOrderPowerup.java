489435d2441437f6520fe4e32f89ff7125fc7d52
Pangreor <64980093+Pangreor@users.noreply.github.com>
2023-03-11 13:25:04 +0000

    Potential fix for gradle build

--- core/src/Sprites/ResetOrderPowerup.java
+++ core/src/Sprites/ResetOrderPowerup.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class ResetOrderPowerup extends Powerup {
     public ResetOrderPowerup( float x, float y){
-        super(new Texture(Gdx.files.internal("Powerups/resetPowerup.png")), x, y);
+        super(new Texture("Powerups/resetPowerup.png"), x, y);
     }
 
 }
