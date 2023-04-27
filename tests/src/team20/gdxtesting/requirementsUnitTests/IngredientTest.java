e2fecdfbc1ba324b16cc177183d353edb913110a
CrimsonLeaves <109231078+CrimsonLeaves@users.noreply.github.com>
2023-04-27 13:40:58 +0100

    Amending Ingredients and Tests
    Changing the setSkin from || to && as otherwise it will never return anything as it cannot be both 0 and 1.
    Adding a test to IngredientTest to test for an invalid input.
    
    Signed-off-by: CrimsonLeaves <109231078+CrimsonLeaves@users.noreply.github.com>

--- tests/src/team20/gdxtesting/requirementsUnitTests/IngredientTest.java
+++ tests/src/team20/gdxtesting/requirementsUnitTests/IngredientTest.java
@@ -69,6 +69,13 @@ public class IngredientTest {
         //Tests if the ingredient can instead be displayed as prepared by changing the image.
         potato.setSkin(1);
         assertTrue("The potato skin should be set to 1", potato.getSkin()==1);
+        //Test that skin can be changed back to 0.
+        potato.setSkin(0);
+        assertTrue("The potato skin should be set to 0", potato.getSkin()==0);
+        //Test that the skin can't be set to an invalid value.
+        potato.setSkin(2);
+        assertFalse("The potato skin cannot be set anything but 1 or 0"
+                , potato.getSkin()==2);
     }
     @Test
     //Test Requirement: UR_FOOD_PREP/UR_INGREDIENT_BEANS
