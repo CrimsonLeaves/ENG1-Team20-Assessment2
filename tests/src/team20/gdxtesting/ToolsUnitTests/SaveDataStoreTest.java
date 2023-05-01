c94cc2d0e49d69850a486b014dc401e131e0be41
CrimsonLeaves <109231078+CrimsonLeaves@users.noreply.github.com>
2023-05-01 20:42:25 +0100

    Finished setters for SaveDataStore Test
    Finished writing all the setters for the SaveDataStore Test
    
    Signed-off-by: CrimsonLeaves <109231078+CrimsonLeaves@users.noreply.github.com>

--- tests/src/team20/gdxtesting/ToolsUnitTests/SaveDataStoreTest.java
+++ tests/src/team20/gdxtesting/ToolsUnitTests/SaveDataStoreTest.java
@@ -1,9 +1,14 @@
 package team20.gdxtesting.ToolsUnitTests;
 
+import Tools.ChefDataStore;
+import Tools.OrderDataStore;
 import Tools.SaveDataStore;
+import com.team13.piazzapanic.Reputation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import team20.gdxtesting.GdxTestRunner;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -17,17 +22,23 @@ public class SaveDataStoreTest {
     //Test Requirement: UR_SAVE_GAME/UR_CONTINUE_GAME
     public void test_ChefDataStore(){
         //As only setters are used, the test will check if the data is correctly stored.
-        SaveDataStore store = new SaveDataStore();
-        assertTrue(store.getChefData().isEmpty());
-        assertTrue(store.getDiffMult()==1f);
-        assertSame(store.getOrderCount(), 0);
+        ArrayList<ChefDataStore> chefStore = new ArrayList<>();
+        Reputation rep = new Reputation();
+        OrderDataStore orderStore = new OrderDataStore();
+        SaveDataStore store = new SaveDataStore(chefStore, 5, 2f,
+                false, 10f, 15f, 3, 2, rep,
+                orderStore, null);
+
+        assertSame(store.getChefData(), chefStore);
+        assertTrue(store.getDiffMult()==2f);
+        assertSame(store.getOrderCount(), 5);
         assertFalse(store.getCreatedOrder());
-        assertTrue(store.getTimeSeconds()==0f);
-        assertTrue(store.getTimeSecondsCount()==0f);
+        assertTrue(store.getTimeSeconds()==10f);
+        assertTrue(store.getTimeSecondsCount()==15f);
         assertSame(store.getChefCount(), 3);
-        assertSame(store.getScore(), 0);
-
-
+        assertSame(store.getScore(), 2);
+        assertSame(rep, store.getRep());
+        assertSame(orderStore, store.getOrder());
         assertNull(store.getStationItems());
     }
 }
