8adebd6c4d9b110e8e9477e4d50f4bbb2588b7f7
laurenvictoriaa <117826360+laurenvictoriaa@users.noreply.github.com>
2023-05-01 21:16:45 +0100

    Finished CircularList tests

--- tests/src/team20/gdxtesting/ToolsUnitTests/TestCircularList.java
+++ tests/src/team20/gdxtesting/ToolsUnitTests/TestCircularList.java
@@ -23,7 +23,7 @@ public class TestCircularList {
         /**
          * Tests that a new instance of a circular list is instantiated correctly
          */
-        CircularList cList = new CircularList();
+        CircularList cList = new CircularList<Chef>();
         assertEquals(cList.getCurrentSize(), 0);
         assertEquals(cList.allElems(), new ArrayList<>());
     }
@@ -34,7 +34,7 @@ public class TestCircularList {
         /**
          * Tests adding a new item to the circular list
          */
-        CircularList cList = new CircularList();
+        CircularList cList = new CircularList<Chef>();
         Chef testChef = new Chef(new World(new Vector2(0,0),true), 0, 0);
         cList.addElement(testChef);
         assertEquals(cList.getCurrentSize(), 1);
@@ -46,12 +46,12 @@ public class TestCircularList {
         /**
          * Tests removing an item from the circular list
          */
-        CircularList cList = new CircularList();
+        CircularList cList = new CircularList<Chef>();
         Chef testChef = new Chef(new World(new Vector2(0,0),true), 0, 0);
         cList.addElement(testChef);
         cList.removeElement();
         assertEquals(cList.getCurrentSize(), 0);
-        assertEquals(cList.peekNextItem(), null);
+        assertNull(cList.peekNextItem());
         assertTrue(cList.isEmpty());
     }
 
@@ -60,12 +60,9 @@ public class TestCircularList {
         /**
          * Tests removing an item from an empty circular list
          */
-        CircularList cList = new CircularList();
-        assertEquals(cList.removeElement(), null);
-    }
-
-    public void testAddingItemAboveMaxSize() {
-        CircularList cList = new CircularList();
+        CircularList cList = new CircularList<Chef>();
+        assertNull(cList.removeElement());
+        assertEquals(cList.getCurrentSize(), 0);
     }
 
     @Test
@@ -73,22 +70,74 @@ public class TestCircularList {
         /**
          * Tests functionality of the isEmpty method
          */
-        CircularList cList = new CircularList();
-        assertEquals(cList.isEmpty(), true);
+        CircularList cList = new CircularList<Chef>();
+        assertTrue(cList.isEmpty());
         cList.addElement(new Chef(new World(new Vector2(0,0),true), 0, 0));
-        assertEquals(cList.isEmpty(), false);
+        assertFalse(cList.isEmpty());
     }
+
+    @Test
+    public void testNextItem() {
+        /**
+         * Tests functionality of the nextItem method
+         */
+        CircularList cList = new CircularList<Chef>();
+        Chef testChef = new Chef(new World(new Vector2(0,0),true), 0, 0);
+        Chef testChef2 = new Chef(new World(new Vector2(1,0),true), 0, 0);
+
+        cList.addElement(testChef);
+        assertEquals(cList.peekNextItem(), testChef);
+        cList.addElement(testChef2);
+        assertEquals(cList.peekNextItem(),testChef);
+        cList.nextItem();
+        assertEquals(cList.peekNextItem(),testChef2);
+        cList.nextItem();
+        assertEquals(cList.peekNextItem(),testChef);
+    }
+
 
     @Test
     public void testPeekNextItem() {
         /**
          * Tests functionality of the peekNextItem method
          */
-        CircularList cList = new CircularList();
-        assertEquals(cList.isEmpty(), true);
-        cList.addElement(new Chef(new World(new Vector2(0,0),true), 0, 0));
-        assertEquals(cList.isEmpty(), false);
+        CircularList cList = new CircularList<Chef>();
+        Chef testChef = new Chef(new World(new Vector2(0,0),true), 0, 0);
+        Chef testChef2 = new Chef(new World(new Vector2(1,0),true), 0, 0);
+
+        cList.addElement(testChef);
+        assertEquals(cList.peekNextItem(), testChef);
+        cList.addElement(testChef2);
+        assertEquals(cList.peekNextItem(),testChef);
+        cList.nextItem();
+        assertEquals(cList.peekNextItem(),testChef2);
     }
+
+    @Test
+    public void testGetCurrentSize() {
+        /**
+         * Tests functionality of the getCurrentSize method
+         */
+        CircularList cList = new CircularList<Chef>();
+        Chef testChef = new Chef(new World(new Vector2(0, 0), true), 0, 0);
+        assertEquals(cList.getCurrentSize(), 0);
+        cList.addElement(testChef);
+        assertEquals(cList.getCurrentSize(),1);
+        cList.addElement(testChef);
+        assertEquals(cList.getCurrentSize(), 2);
+        cList.removeElement();
+        assertEquals(cList.getCurrentSize(), 1);
+    }
+
+    @Test
+    public void testAllElems() {
+        /**
+         * Tests functionality of the allElems method
+         */
+        CircularList cList = new CircularList<Integer>();
+        assertEquals(cList.allElems(), java.util.Collections.emptyList());
+    }
+
 
 
 }
