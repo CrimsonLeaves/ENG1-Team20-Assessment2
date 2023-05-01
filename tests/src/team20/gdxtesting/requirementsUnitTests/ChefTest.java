bfbace237c0510346bb9d1d5b8042cdb0b2ed13b
CrimsonLeaves <109231078+CrimsonLeaves@users.noreply.github.com>
2023-05-01 18:03:55 +0100

    Updating Chef tests
    Testing Chefs colliding method.
    
    Signed-off-by: CrimsonLeaves <109231078+CrimsonLeaves@users.noreply.github.com>

--- tests/src/team20/gdxtesting/requirementsUnitTests/ChefTest.java
+++ tests/src/team20/gdxtesting/requirementsUnitTests/ChefTest.java
@@ -2,19 +2,14 @@ package team20.gdxtesting.requirementsUnitTests;
 import Ingredients.Cheese;
 import Ingredients.Tomato;
 import Sprites.*;
-import Tools.CircularList;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.World;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import team20.gdxtesting.GdxTestRunner;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
 
@@ -23,7 +18,9 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(GdxTestRunner.class)
 public class ChefTest {
-
+    /**
+     * Testing the Chef class.
+     */
     @Test
     //Test requirement: UR_CARRYING_STACK
     public void test_initialEmpty(){
@@ -38,7 +35,6 @@ public class ChefTest {
         //Add a valid item to the stack.
         Tomato t = new Tomato(new HashMap<String, Float>(), new HashMap<String, Boolean>());
         Chef c = new Chef(new World(new Vector2(0,0),true), 0, 0);
-        Integer initial = c.getHoldingSize();
         c.pickUp(t);
         Integer size = c.getHoldingSize();
         assertTrue("One valid item should be added to the stack.", size.equals(1));
@@ -62,7 +58,6 @@ public class ChefTest {
         Cheese cheese = new Cheese(new HashMap<String, Float>(), new HashMap<String, Boolean>());
         c.pickUp(cheese);
         //Cheese should be at the top.
-        Deque<Sprite> stack = c.getStack();
         assertTrue("The last item to be added should be at the top.", cheese.equals(c.peekStack()));
 
     }
@@ -71,13 +66,15 @@ public class ChefTest {
     public void test_pickUp_fullStack(){
         //Tests if nothing will be added to the stack if it is already full.
         //Create a new stack and fill it (so it contains 3 items).
-        Chef c = new Chef(new World(new Vector2(0,0),true), 0, 0);
+        Chef chef = new Chef(new World(new Vector2(0,0),true), 0, 0);
         Tomato t = new Tomato(new HashMap<String, Float>(), new HashMap<String, Boolean>());
+        Deque<Sprite> stack = new ArrayDeque<>();
         for (int i = 0; i<3; i++){
-            c.pickUp(t);
+            stack.add(t);
         }
-        Integer size = c.getHoldingSize();
-        c.pickUp(t);
+        chef.setStack(stack);
+        Integer size = chef.getHoldingSize();
+        chef.pickUp(t);
         assertTrue("Once the stack is full another item shouldn't be added.", size.equals(3));
     }
     @Test
@@ -92,22 +89,22 @@ public class ChefTest {
         assertTrue("The top item of the stack should be removed.", s.equals(c.peekStack()));
     }
     @Test
-    //Test Requirement: FR_SWITCH
-    public void test_getUserControlChef(){
-        //Tests if the systems knows which chef is being controlled.
-        Chef c1 = new Chef(new World(new Vector2(0,0),true), 0, 0);
-        Chef c2 = new Chef(new World(new Vector2(0,0),true), 0, 0);
-        Chef c3 = new Chef(new World(new Vector2(0,0),true), 0, 0);
-        CircularList<Chef> chefList = new CircularList<>(4);
-        chefList.addElement(c1);
-        chefList.addElement(c2);
-        chefList.addElement(c3);
-        assertTrue("The last chef in the list by default should be initially controlled.",
-                c3.getUserControlChef());
+    //Test Requirement: FR_INTERACT
+    public void test_chefsColliding(){
+        //Tests that the chef updates its self when colliding with another chef.
+        Chef chef = new Chef(new World(new Vector2(0,0),true), 0, 0);
+        //Before colliding
+        assertTrue("The user should be able to control the chef if it isn't colliding."
+                , chef.getUserControlChef());
+        assertFalse("The chef shouldn't be set as colliding."
+                , chef.chefOnChefCollision);
+        //After colliding.
+        chef.chefsColliding();
+        assertFalse("The user shouldn't be able to control the chef if it is colliding."
+                , chef.getUserControlChef());
+        assertTrue("The chef should be set as colliding."
+                , chef.chefOnChefCollision);
 
-
-       // assertFalse("As the last chef in the list is being controlled, the other chefs shouldn't be controlled.",
-       //         c2.getUserControlChef());
     }
 
     @Test
@@ -117,13 +114,6 @@ public class ChefTest {
         Chef c1 = new Chef(new World(new Vector2(0,0),true), 0, 0);
         assertTrue("If the chef isn't touching anything it should return null.",
                 c1.getTouchingTile()==null);
-        //If the chef is touching a tile, it should return that tile.
-        IngredientStation i = new IngredientStation(new World(new Vector2(1,1),true), new TiledMap(), new BodyDef(), new Rectangle());
-        c1.setX(1);
-        c1.setY(1);
-        c1.update(0);
-        assertTrue(c1.getTouchingTile()==null);
-        //Should be failing, obviously not colliding properly.
+
     }
-    //Need a test to deal with Chefs colliding with each other.
 }
