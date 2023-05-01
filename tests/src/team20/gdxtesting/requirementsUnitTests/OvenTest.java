b85475656ffce400495f9d25c11bd74872ddbad8
CrimsonLeaves <109231078+CrimsonLeaves@users.noreply.github.com>
2023-05-01 17:24:09 +0100

    Finished Oven tests.
    Adding testing for the Oven update method.
    
    Signed-off-by: CrimsonLeaves <109231078+CrimsonLeaves@users.noreply.github.com>

--- tests/src/team20/gdxtesting/requirementsUnitTests/OvenTest.java
+++ tests/src/team20/gdxtesting/requirementsUnitTests/OvenTest.java
@@ -1,8 +1,13 @@
 package team20.gdxtesting.requirementsUnitTests;
 
+import Ingredients.Beans;
+import Ingredients.FailedIngredient;
 import Ingredients.Steak;
 import Recipe.BurgerRecipe;
+import Recipe.CookedPizzaRecipe;
+import Recipe.UncookedPizzaRecipe;
 import Sprites.Oven;
+import Tools.Constants;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
@@ -73,6 +78,80 @@ public class OvenTest {
     @Test
     //Test Requirement: UR_FOOD_PREP/UR_RECIPE/FR_NO_BURN_POWERUP/FR_INSTACOOK_POWERUP
     public void test_update(){
-        //
+        //Change the current state of the oven.
+        Oven oven = new Oven(new World(new Vector2(0,0),true), new TiledMap(),
+                new BodyDef(), new Rectangle());
+        oven.setTimer(10f);
+        //No recipe and no ingredient so nothing should change.
+        oven.update(10f, 10f, false, false);
+        assertTrue("No ingredient or recipe so the timer should remain unchanged."
+                , oven.getTimer()==10f);
+        //Failed ingredient.
+        oven.setCurrentIngredient(new FailedIngredient());
+        oven.update(10f, 10f, false, false);
+        assertTrue("The ingredient is failed so nothing should be updated."
+                , oven.getTimer()==10f);
+
+        //Correct ingredient.
+        //Branch 1 Apply the instant cook power up to the ingredient.
+        HashMap<String, Float> timers = new HashMap<>();
+        HashMap<String, Boolean> completed = new HashMap<>();
+        timers.put(Constants.OVEN, 30f);
+        completed.put(Constants.OVEN, false);
+        Beans beans = new Beans(timers, completed);
+        oven.setCurrentIngredient(beans);
+        oven.update(10f, 10f, true, false);
+        assertTrue("The beans should be instantly cooked and so the timer should be reset to 0."
+                , oven.getTimer()==0f);
+        assertTrue("The beans should be instantly cooked and so set as completed."
+                , oven.getCurrentIngredient().isCompleted(Constants.OVEN));
+        //Branch 2 The ingredient is cooked without the instant cook power up.
+        completed.put(Constants.OVEN, false);
+        oven.setTimer(40f);
+        oven.update(10f, 10f, false, false);
+        assertTrue("The beans should be cooked and so the timer should be reset to 0."
+                , oven.getTimer()==0f);
+        assertTrue("The beans should be cooked and so set as completed."
+                , oven.getCurrentIngredient().isCompleted(Constants.OVEN));
+        //Branch 3 Apply the no burn power up to the ingredient's timer.
+        oven.setTimer(40f);
+        oven.update(10f, 10f, false, true);
+        assertTrue("The timer should be reset to 0 with the no burn power up."
+                , oven.getTimer()==0f);
+        //Branch 4 The ingredient is overcooked.
+        oven.setTimer(100f);
+        oven.update(1, 1, false, false);
+        assertTrue("The ingredient should fail if it is overcooked."
+                , oven.getCurrentIngredient() instanceof FailedIngredient);
+
+        //No ingredient, there is a recipe.
+        oven.setTimer(0f);
+        oven.setCurrentIngredient(null);
+        UncookedPizzaRecipe uncookedPizza = new UncookedPizzaRecipe();
+        //Branch 1 no burn power up for the uncooked pizza.
+        oven.setCurrentRecipe(uncookedPizza);
+        oven.update(1, 1, true, false);
+        assertTrue("The uncooked pizza should be instantly cooked and the timer set to 0."
+                , oven.getTimer()==0f);
+        assertTrue("The uncooked pizza should be instantly cooked and turned into a cooked pizza."
+                , oven.getCurrentRecipe()instanceof CookedPizzaRecipe);
+        //Branch 2 Cooking the uncooked pizza without instant cook power up.
+        oven.setCurrentRecipe(uncookedPizza);
+        oven.update(10, 1, false, false);
+        assertTrue("The uncooked pizza should be cooked and the timer set to 0."
+                , oven.getTimer()==0f);
+        assertTrue("The uncooked pizza should be cooked and turned into a cooked pizza."
+                , oven.getCurrentRecipe()instanceof CookedPizzaRecipe);
+        //Branch 3 Applying the no burn power up to the pizza.
+        oven.update(1, 1, false, true);
+        assertTrue("The timer should be reset to 0 with the no burn power up.",
+                oven.getTimer()==0f);
+        //Branch 4 Cooked pizza is overcooked and so becomes a failed ingredient.
+        oven.setTimer(1f);
+        oven.update(1, 0, false, false);
+        assertNull("The cooked pizza has been overcooked, turned into a failed ingredient"
+                + "and so the recipe should be null.", oven.getCurrentRecipe());
+        assertTrue("The cooked pizza has been overcooked and should be failed."
+                , oven.getCurrentIngredient() instanceof FailedIngredient);
     }
 }
